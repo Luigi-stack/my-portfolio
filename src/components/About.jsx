0f6369d434628d85577c8ec0753f0180cd879e4d
Luigi Iossa <luigi_iossa@hotmail.com>
2023-09-27 14:53:24 +0000

    responsive

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,14 +5,26 @@ function About() {
         <>
             <Container id="about" className="justify-content-center mt-5">
                 <Row>
-                    <Col xs={12} md={6}  className="">
-                        <h2 className="my-5">Who is Luigi?</h2>
+                    <Col xs={12} md={6} className="">
+                        <h2 className="sm my-5 d-md-none">Who is Luigi?</h2>
+
+                        <h2 className="md my-5 d-none d-md-block d-lg-none">Who is Luigi?</h2>
+
+                        <h2 className="lg my-5 d-none d-lg-block d-xl-none">Who is Luigi?</h2>
+
+                        <h2 className="xl my-5 d-none d-xl-block">Who is Luigi?</h2>
                         {/* eslint-disable-next-line react/no-unescaped-entities */}
                         <p className="p_about">I'm a Jr. Web Developer. Strong creative and analytical skills. Team player with an eye for detail. After working in the field of international cooperation for a few years I became interested in software development and decided to join the full stack development bootcamp at Epicode School.</p>
                         <hr className="border border-white opacity-100 border-1" />
                     </Col>
-                    <Col xs={12} md={6}  className="d-flex flex-column justify-content-center align-items-center">
-                        <img className="mb-4" src="https://raw.githubusercontent.com/Luigi-stack/Portfolio/main/src/assets/coding.png" alt="photo profile" width={600} />
+                    <Col xs={12} md={6} className="d-flex flex-column justify-content-center align-items-center">
+
+                        <img className="mb-4 d-none d-md-block d-lg-none" src="https://raw.githubusercontent.com/Luigi-stack/Portfolio/main/src/assets/coding.png" alt="photo profile" width={385} />
+
+                        <img className="mb-4 d-none d-lg-block" src="https://raw.githubusercontent.com/Luigi-stack/Portfolio/main/src/assets/coding.png" alt="photo profile" width={600} />
+
+                        <img className="mb-4 d-md-none" src="https://raw.githubusercontent.com/Luigi-stack/Portfolio/main/src/assets/coding.png" alt="photo profile" width={600} />
+
                         <p className="ms-5">
                             <a href="" className="link-offset-2 link-underline link-underline-opacity-0"><i className="bi bi-arrow-return-right text-white fs-5"> Download my Curriculum</i></a>
                         </p>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,14 +8,38 @@ function HomePage() {
             <Container id="home" className="home justify-content-center">
                 <Row>
                     <Col xs={12} md={6} className="d-flex flex-column justify-content-center align-items-center">
-                        <h1>Welcome to
+
+                        <h1 className="sm d-md-none">Welcome to
                             <br />
                             My Portoflio
                         </h1>
-                        <a href="#about" className="btn btn-outline-light mt-4 fs-5 p-3 px-4 rounded-pill">Luigi Iossa / Web Developer</a>
+
+                        <h1 className="md d-none d-md-block d-lg-none">Welcome to
+                            <br />
+                            My Portoflio
+                        </h1>
+
+                        <h1 className="lg d-none d-lg-block d-xl-none">Welcome to
+                            <br />
+                            My Portoflio
+                        </h1>
+
+                        <h1 className="xl d-none d-xl-block">Welcome to
+                            <br />
+                            My Portoflio
+                        </h1>
+
+                        <a href="#about" className="btn btn-outline-light mt-4 p-3 px-4 rounded-pill">Luigi Iossa / Web Developer</a>
                     </Col>
+
                     <Col xs={12} md={6} className="d-flex flex-column justify-content-center align-items-center">
-                        <img className="mb-4" src="https://github.com/Luigi-stack/Portfolio/blob/main/src/assets/photoprofile.png?raw=true" alt="photo profile" width={600} />
+
+                        <img className="mb-4 d-none d-md-block d-lg-none" src="https://github.com/Luigi-stack/Portfolio/blob/main/src/assets/photoprofile.png?raw=true" alt="photo profile" width={385} />
+
+                        <img className="mb-4 d-none d-lg-block" src="https://github.com/Luigi-stack/Portfolio/blob/main/src/assets/photoprofile.png?raw=true" alt="photo profile" width={600} />
+                        
+                        <img className="mb-4 d-md-none" src="https://github.com/Luigi-stack/Portfolio/blob/main/src/assets/photoprofile.png?raw=true" alt="photo profile" width={600} />
+
                         <p>
                             <a href="" className="link-offset-2 link-underline link-underline-opacity-0"><i className="bi bi-arrow-return-right text-white fs-5"> Work with me today</i></a>
                         </p>
