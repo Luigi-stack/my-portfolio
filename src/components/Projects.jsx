bc639784cd7b7b55f706035065c62c52bc1edb04
Luigi Iossa <luigi_iossa@hotmail.com>
2023-09-27 15:29:19 +0000

    responsive

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,29 +7,42 @@ function Projects() {
             <Container id="projects" className="mt-5">
                 <Row>
                     <Col>
-                        <h2 className="my-5"> My Projects</h2>
+                        <h2 className="sm my-5 d-md-none">My Projects</h2>
+
+                        <h2 className="md my-5 d-none d-md-block d-lg-none">My Projects</h2>
+
+                        <h2 className="lg my-5 d-none d-lg-block d-xl-none">My Projects</h2>
+
+                        <h2 className="xl my-5 d-none d-xl-block">My Projects</h2>
                     </Col>
                 </Row>
 
                 <Container>
 
 
-                    <Row className="mb-5">
-                        <Col>
+                    <Row>
+                        <Col md={6} lg={3} className="d-flex flex-column align-items-center">
                             <img src="src/assets/Blue and Orange Modern Professional Business Conference Instagram Post.png" alt="" width={200} />
                             <p>
                                 <a href="" className="link-offset-2 link-underline link-underline-opacity-0"><i className="p_projects bi bi-arrow-return-right text-white fs-5"> Download my Curriculum</i></a>
                             </p>
                         </Col>
 
-                        <Col>
+                        <Col md={6} lg={3} className="d-flex flex-column align-items-center">
                             <img src="src/assets/Blue and Orange Modern Professional Business Conference Instagram Post.png" alt="" width={200} />
                             <p>
                                 <a href="" className="link-offset-2 link-underline link-underline-opacity-0"><i className="p_projects bi bi-arrow-return-right text-white fs-5"> Download my Curriculum</i></a>
                             </p>
                         </Col>
 
-                        <Col>
+                        <Col md={6} lg={3} className="d-flex flex-column align-items-center">
+                            <img src="src/assets/Blue and Orange Modern Professional Business Conference Instagram Post.png" alt="" width={200} />
+                            <p>
+                                <a href="" className="link-offset-2 link-underline link-underline-opacity-0"><i className="p_projects bi bi-arrow-return-right text-white fs-5"> Download my Curriculum</i></a>
+                            </p>
+                        </Col>
+
+                        <Col md={6} lg={3} className="d-flex flex-column align-items-center">
                             <img src="src/assets/Blue and Orange Modern Professional Business Conference Instagram Post.png" alt="" width={200} />
                             <p>
                                 <a href="" className="link-offset-2 link-underline link-underline-opacity-0"><i className="p_projects bi bi-arrow-return-right text-white fs-5"> Download my Curriculum</i></a>
@@ -38,21 +51,28 @@ function Projects() {
                     </Row>
 
                     <Row>
-                        <Col>
+                        <Col md={6} lg={3} className="d-flex flex-column align-items-center">
                             <img src="src/assets/Blue and Orange Modern Professional Business Conference Instagram Post.png" alt="" width={200} />
                             <p>
                                 <a href="" className="link-offset-2 link-underline link-underline-opacity-0"><i className="p_projects bi bi-arrow-return-right text-white fs-5"> Download my Curriculum</i></a>
                             </p>
                         </Col>
 
-                        <Col>
+                        <Col md={6} lg={3} className="d-flex flex-column align-items-center">
                             <img src="src/assets/Blue and Orange Modern Professional Business Conference Instagram Post.png" alt="" width={200} />
                             <p>
                                 <a href="" className="link-offset-2 link-underline link-underline-opacity-0"><i className="p_projects bi bi-arrow-return-right text-white fs-5"> Download my Curriculum</i></a>
                             </p>
                         </Col>
 
-                        <Col>
+                        <Col md={6} lg={3} className="d-flex flex-column align-items-center">
+                            <img src="src/assets/Blue and Orange Modern Professional Business Conference Instagram Post.png" alt="" width={200} />
+                            <p>
+                                <a href="" className="link-offset-2 link-underline link-underline-opacity-0"><i className="p_projects bi bi-arrow-return-right text-white fs-5"> Download my Curriculum</i></a>
+                            </p>
+                        </Col>
+
+                        <Col md={6} lg={3} className="d-flex flex-column align-items-center">
                             <img src="src/assets/Blue and Orange Modern Professional Business Conference Instagram Post.png" alt="" width={200} />
                             <p>
                                 <a href="" className="link-offset-2 link-underline link-underline-opacity-0"><i className="p_projects bi bi-arrow-return-right text-white fs-5"> Download my Curriculum</i></a>
