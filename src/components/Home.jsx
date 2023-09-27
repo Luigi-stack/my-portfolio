c62276f8796fa3674aa2d97517bc0601749c9036
Luigi Iossa <luigi_iossa@hotmail.com>
2023-09-27 13:35:44 +0000

    image

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -15,7 +15,7 @@ function HomePage() {
                         <a href="#about" className="btn btn-outline-light mt-4 fs-5 p-3 px-4 rounded-pill">Luigi Iossa / Web Developer</a>
                     </Col>
                     <Col xs={12} md={6} className="d-flex flex-column justify-content-center align-items-center">
-                        <img className="mb-4" src="src/assets/photoprofile.png" alt="photo profile" width={600} />
+                        <img className="mb-4" src="https://github.com/Luigi-stack/Portfolio/blob/main/src/assets/photoprofile.png?raw=true" alt="photo profile" width={600} />
                         <p>
                             <a href="" className="link-offset-2 link-underline link-underline-opacity-0"><i className="bi bi-arrow-return-right text-white fs-5"> Work with me today</i></a>
                         </p>
