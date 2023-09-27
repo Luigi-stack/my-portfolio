76a465c996ad97b57413c44c525d5d1d9869fee6
Luigi Iossa <luigi_iossa@hotmail.com>
2023-09-27 13:33:09 +0000

    immagine

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,7 +12,7 @@ function About() {
                         <hr className="border border-white opacity-100 border-1" />
                     </Col>
                     <Col xs={12} md={6}  className="d-flex flex-column justify-content-center align-items-center">
-                        <img className="mb-4" src="src/assets/coding.png" alt="photo profile" width={600} />
+                        <img className="mb-4" src="https://raw.githubusercontent.com/Luigi-stack/Portfolio/main/src/assets/coding.png" alt="photo profile" width={600} />
                         <p className="ms-5">
                             <a href="" className="link-offset-2 link-underline link-underline-opacity-0"><i className="bi bi-arrow-return-right text-white fs-5"> Download my Curriculum</i></a>
                         </p>
