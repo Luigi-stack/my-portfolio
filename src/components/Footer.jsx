c0205a94dbc3c4363490077de2299d556fe9396a
Luigi Iossa <luigi_iossa@hotmail.com>
2023-10-16 18:08:05 +0000

    footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -28,9 +28,9 @@ function Footer() {
                 </Row>
                 <Row>
                     <Col>
-                        <Link to="https://github.com/Luigi-stack/" className="text-secondary fs-3 me-4 link-offset-2 link-underline link-underline-opacity-0" target="_blank" rel="noopener noreferrer"><AiFillGithub /></Link>
-                        <Link to="https://www.linkedin.com/in/luigi-iossa-fullstack-developer/" className="text-secondary fs-3 me-4 link-offset-2 link-underline link-underline-opacity-0" target="_blank" rel="noopener noreferrer"><AiFillLinkedin /></Link>
-                        <Link to="https://www.instagram.com/luluilu/" className="text-secondary fs-3 link-offset-2 link-underline link-underline-opacity-0" target="_blank" rel="noopener noreferrer"><AiFillInstagram /></Link>
+                        <Link to="https://github.com/Luigi-stack/" className="text-secondary fs-3 me-4 link-offset-2 link-underline link-underline-opacity-0 icon_footer" target="_blank" rel="noopener noreferrer"><AiFillGithub /></Link>
+                        <Link to="https://www.linkedin.com/in/luigi-iossa-fullstack-developer/" className="text-secondary fs-3 me-4 link-offset-2 link-underline link-underline-opacity-0 icon_footer" target="_blank" rel="noopener noreferrer"><AiFillLinkedin /></Link>
+                        <Link to="https://www.instagram.com/luluilu/" className="text-secondary fs-3 link-offset-2 link-underline link-underline-opacity-0 icon_footer" target="_blank" rel="noopener noreferrer"><AiFillInstagram /></Link>
                     </Col>
                 </Row>
             </Container>
