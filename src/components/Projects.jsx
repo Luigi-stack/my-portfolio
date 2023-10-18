0809cf0d2d388fd757a23a5ab8aca011c425b8f2
Luigi Iossa <luigi_iossa@hotmail.com>
2023-10-18 11:50:33 +0000

    modifiche project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -33,39 +33,62 @@ function Projects() {
                 <Container>
                     <Row>
                         <Col md={6} lg={4} className="d-flex flex-column align-items-center mb-5 hoveproject">
-                            <img src={ecommerceImg} alt="ecommerce login image" className="img_projects mb-2" width={250} />
-                            <p>
-                                <Link to="https://github.com/Luigi-stack/capstone_project_frontend.git" target="_blank" rel="noopener noreferrer" className="link-offset-2 link-underline link-underline-opacity-0"><i className="bi bi-arrow-return-right text-white fs-5"> E-commerce Project</i></Link>
-                            </p>
+
+                            <Link to="https://github.com/Luigi-stack/capstone_project_frontend.git" target="_blank" rel="noopener noreferrer" className="link-offset-2 link-underline link-underline-opacity-0">
+
+                                <img src={ecommerceImg} alt="ecommerce login image" className="img_projects mb-2" width={250} />
+                                <p className="d-flex justify-content-center">
+                                    <i className="bi bi-arrow-return-right text-white fs-4"> E-commerce Project</i>
+                                </p>
+                            </Link>
+
                         </Col>
 
 
                         <Col md={6} lg={4} className="d-flex flex-column align-items-center mb-5 hoveproject">
-                            <img src={linkedinCloneImg} alt="profile linkedin image" className="img_projects mb-2" width={250} />
-                            <p>
-                                <Link to="https://github.com/Luigi-stack/LinkedIn-Build-week3.git" target="_blank" rel="noopener noreferrer" className="link-offset-2 link-underline link-underline-opacity-0"><i className="bi bi-arrow-return-right text-white fs-5"> Linkedin Clone</i></Link>
-                            </p>
+
+                            <Link to="https://github.com/Luigi-stack/LinkedIn-Build-week3.git" target="_blank" rel="noopener noreferrer" className="link-offset-2 link-underline link-underline-opacity-0">
+                                <img src={linkedinCloneImg} alt="profile linkedin image" className="img_projects mb-2" width={250} />
+                                <p className="d-flex justify-content-center">
+                                    <i className="bi bi-arrow-return-right text-white fs-4"> Linkedin Clone</i>
+                                </p>
+                            </Link>
+
                         </Col>
 
                         <Col md={6} lg={4} className="d-flex flex-column align-items-center mb-5 hoveproject">
-                            <img src={spotifyCloneImg} alt="home page spotify image" className="img_projects mb-2" width={250} />
-                            <p>
-                                <Link to="https://github.com/Luigi-stack/projectw3-React-spotify.git" target="_blank" rel="noopener noreferrer" className="link-offset-2 link-underline link-underline-opacity-0"><i className="bi bi-arrow-return-right text-white fs-5"> Spotify Clone</i></Link>
-                            </p>
+
+                            <Link to="https://github.com/Luigi-stack/projectw3-React-spotify.git" target="_blank" rel="noopener noreferrer" className="link-offset-2 link-underline link-underline-opacity-0">
+
+                                <img src={spotifyCloneImg} alt="home page spotify image" className="img_projects mb-2" width={250} />
+                                <p className="d-flex justify-content-center">
+                                    <i className="bi bi-arrow-return-right text-white fs-4"> Spotify Clone</i>
+                                </p>
+                            </Link>
+
                         </Col>
 
                         <Col md={6} lg={4} className="d-flex flex-column align-items-center mb-5 hoveproject">
-                            <img src={netflixCloneImg} alt="home page netflix image" className="img_projects mb-2" width={250} />
-                            <p>
-                                <Link to="https://github.com/Luigi-stack/capstone_project_frontend.git" target="_blank" rel="noopener noreferrer" className="link-offset-2 link-underline link-underline-opacity-0"><i className="bi bi-arrow-return-right text-white fs-5"> Netflix Clone</i></Link>
-                            </p>
+
+                            <Link to="https://github.com/Luigi-stack/capstone_project_frontend.git" target="_blank" rel="noopener noreferrer" className="link-offset-2 link-underline link-underline-opacity-0">
+                                <img src={netflixCloneImg} alt="home page netflix image" className="img_projects mb-2" width={250} />
+                                <p className="d-flex justify-content-center">
+                                    <i className="bi bi-arrow-return-right text-white fs-4"> Netflix Clone</i>
+                                </p>
+                            </Link>
+
                         </Col>
 
                         <Col md={6} lg={4} className="d-flex flex-column align-items-center hoveproject">
-                            <img src={weatherAppImg} alt="home page weather app image" className="img_projects mb-2" width={250} />
-                            <p>
-                                <Link to="https://github.com/Luigi-stack/weather-app.git" target="_blank" rel="noopener noreferrer" className="link-offset-2 link-underline link-underline-opacity-0"><i className="bi bi-arrow-return-right text-white fs-5"> Weather App</i></Link>
-                            </p>
+
+                            <Link to="https://github.com/Luigi-stack/weather-app.git" target="_blank" rel="noopener noreferrer" className="link-offset-2 link-underline link-underline-opacity-0">
+
+                                <img src={weatherAppImg} alt="home page weather app image" className="img_projects mb-2" width={250} />
+                                <p className="d-flex justify-content-center">
+                                    <i className="bi bi-arrow-return-right text-white fs-4"> Weather App</i>
+                                </p>
+                            </Link>
+
                         </Col>
 
 
